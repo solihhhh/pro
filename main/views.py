ee8082e8c6730fcb8cf7523f9013bc7d290f357c
Solih <solihhhhhhh@gmail.com>
2024-11-18 19:35:16 +0500

    basic code

--- main/views.py
+++ main/views.py
@@ -39,20 +39,31 @@ def user_logout(request):
 
 
 def product_list(request):
+    query = request.GET.get('q', '')
     sort_by = request.GET.get('sort_by', 'name')
-    if sort_by == 'price_asc':
-        products = Product.objects.all().order_by('price')
-    elif sort_by == 'name':
-        products = Product.objects.all().order_by('name')
+
+    # Qidiruv va sortlash
+    if query:
+        products = Product.objects.filter(name__icontains=query)
     else:
         products = Product.objects.all()
 
-    paginator = Paginator(products, 5)
+    if sort_by == 'price_asc':
+        products = products.order_by('price')
+    elif sort_by == 'name':
+        products = products.order_by('name')
+
+    # Pagination
+    paginator = Paginator(products, 5)  # Har bir sahifada 5 mahsulot
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'main/product_list.html', {'page_obj': page_obj})
-
+    return render(request, 'main/product_list.html', {
+        'products': page_obj,  # Mahsulotlar sahifaga bo'linadi
+        'query': query,
+        'paginator': paginator,
+        'page_obj': page_obj,
+    })
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     likes = Like.objects.filter(product=product)
@@ -61,24 +72,23 @@ def product_detail(request, product_id):
 
 
 @login_required
+def liked_products(request):
+
+    liked_products = Like.objects.filter(user=request.user, liked=True).values_list('product', flat=True)
+    products = Product.objects.filter(id__in=liked_products)
+    return render(request, 'main/liked_products.html', {'liked_products': products})
+
+@login_required
 def like_product(request, product_id):
+
     product = get_object_or_404(Product, id=product_id)
-    user_like = Like.objects.filter(product=product, user=request.user).first()
+    user_like, created = Like.objects.get_or_create(product=product, user=request.user)
 
     if user_like:
         user_like.liked = not user_like.liked
         user_like.save()
-    else:
-        Like.objects.create(product=product, user=request.user, liked=True)
 
-    likes = Like.objects.filter(product=product)
-
-    return render(request, 'main/product_detail.html', {
-        'product': product,
-        'likes': likes,
-        'user_like': user_like,
-    })
-
+    return redirect('liked_products')
 
 @login_required
 def add_comment(request, product_id):
